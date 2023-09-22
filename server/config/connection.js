ce940de88132097664fd9ee9d91ee30b37f2beb0
Humzas <humzas@umich.edu>
2023-09-22 00:22:50 -0400

    first commit

---
+++ server/config/connection.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+// connection to mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/pakdesi",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+module.exports = mongoose.connection;
